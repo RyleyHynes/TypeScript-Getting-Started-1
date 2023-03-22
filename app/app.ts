d030a210f9056331cc029e06d6538cc5104f96c3
Hynes Ryley <HDU4303@hca.corpad.net>
2023-03-22 10:40:52 -0500

    setting up game

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,6 @@
-"hello"
+function startGame() {
+    //start a new game
+    const messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome To MultiMath! Start New Game...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
