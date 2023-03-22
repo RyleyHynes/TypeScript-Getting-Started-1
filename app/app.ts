94c52ef204cb14b4c4fe6e20d2ea5e44ad616366
Hynes Ryley <HDU4303@hca.corpad.net>
2023-03-22 11:59:55 -0500

    project fully set up and moving to module 3

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 function startGame() {
     //start a new game
     const messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome To MultiMath! Start New Game...';
+    messagesElement!.innerText = 'Welcome To MultiMath! Start New Game...';
 }
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './app/app.ts',
+  entry: './app/app.ts', 
   devtool: 'inline-source-map',
   mode: 'development',
   module: {
